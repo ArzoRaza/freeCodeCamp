58b241da9b304340e963a93f62754d41d715438f
ArzoRaza <ccoding76@gmail.com>
2025-03-08 01:54:11 +0530

    JavaScript Initialization

--- script.js
+++
@@ -1,7 +0,0 @@
-function totalWater(waterAmounts) {
-    let Twl = 0;
-    for(let i = 0; i <= waterAmounts.length; i++){
-      Twl += waterAmounts.length[i];
-    }
-    return Twl;
-  }
---
+++ stuff/script.js
@@ -0,0 +1,27 @@
+// Array practice
+
+let arr = [33, 11, 63, 44, 66, 88, 99, 98, 89, 93, 99];
+
+
+// _______________________  Mutator methods _______________________________
+// arr.sort();
+// console.log(arr); //11, 33, 44, 63, 66, 88, 89, 93, 98, 99, 99
+
+// arr.reverse();
+// console.log(arr); // reverce the array
+
+// arr.splice(3, 0, 1);   
+// console.log(arr); // [33, 11, 63, 1, 44, 66, 88, 99, 98, 89, 93, 99]
+
+
+// ________________________  Accessor Methods _____________________________
+
+// let newArr = [50, 40, 30, 20, 10];
+
+// let mergerArr = arr.concat(newArr);
+// console.log(mergerArr);   // [33, 11, 63, 44, 66, 88, 99, 98, 89, 93, 99, 50, 40, 30, 20, 10]
+
+
+// console.log(arr.slice(2, 5));
+
+
